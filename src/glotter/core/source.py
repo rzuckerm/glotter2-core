f32a4e67c0af0cc489c078166d34ad78fde3fcc5
rzuckerm <rzdulc@gmail.com>
2026-02-15 10:13:45 -0600

    Add 'categorize_sources'

--- src/glotter/core/source.py
+++ src/glotter/core/source.py
@@ -1,8 +1,12 @@
 """Source information"""
 
+import os
 from dataclasses import dataclass, field
 from pathlib import Path
 
+import yaml
+
+from glotter.core.project import CoreProject
 from glotter.core.testinfo import TestInfo
 
 
@@ -45,4 +49,73 @@ class CoreSource:
         return "".join(Path(self.filename).suffixes)
 
 
-__all__ = ["CoreSource"]
+@dataclass
+class CoreSourceCategories:
+    """
+    Categories for sources
+
+    :ivar testable_by_project: dictionary whose key is the project type and
+        whose value is a testable source object
+    :ivar by_language: dictionary whose key is the language and whose
+        value is a source object
+    :ivar test_info: dictionary whose key is the language name and whose value
+        is a TestInfo object
+    :ivar bad_sources: list of filenames that do not belong to a project
+    """
+
+    testable_by_project: dict[str, CoreSource] = field(default_factory=dict)
+    by_language: dict[str, CoreSource] = field(default_factory=dict)
+    test_info: dict[str, TestInfo] = field(default_factory=dict)
+    bad_sources: list[str] = field(default_factory=list)
+
+
+_IGNORED_FILENAMES = {"untestable.yml", "testinfo.yml", "README.md"}
+
+
+def categorize_sources(
+    path: str, projects: dict[str, CoreProject], source_cls: type
+) -> CoreSourceCategories:
+    """
+    Categorize sources
+
+    :param path: path to source directory
+    :param projects: dictionary whose key is a project type and whose value is a
+        CoreProject object
+    :param source_cls: source object class
+    :return: CoreSourceCategories object containing information of the source
+        categories
+    """
+
+    categories = CoreSourceCategories()
+    categories.testable_by_project = {k: [] for k in projects}
+    orig_path = Path(path).resolve()
+    for root, _, files in os.walk(path):
+        current_path = Path(root).resolve()
+        if "testinfo.yml" in files:
+            test_info_string = Path(current_path, "testinfo.yml").read_text(encoding="utf-8")
+            language = current_path.name
+            test_info = TestInfo.from_dict(yaml.safe_load(test_info_string), language)
+            categories.test_info[language] = test_info
+            folder_info = test_info.file_info
+            folder_project_names = folder_info.get_project_mappings(
+                projects, include_extension=True
+            )
+            for project_type, project_name in folder_project_names.items():
+                if project_name in files:
+                    source = source_cls(project_name, language, str(current_path), test_info_string)
+                    categories.by_language.setdefault(language, []).append(source)
+                    if source.test_info.is_testable:
+                        categories.testable_by_project[project_type].append(source)
+
+            invalid_filenames = set(files) - (
+                set(folder_project_names.values()) | _IGNORED_FILENAMES
+            )
+            categories.bad_sources += [
+                str(current_path.relative_to(orig_path) / filename)
+                for filename in invalid_filenames
+            ]
+
+    return categories
+
+
+__all__ = ["CoreSource", "CoreSourceCategories", "categorize_sources"]
--- test/test_source.py
+++ test/test_source.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
 import pytest
+import yaml
 
-from glotter.core.source import CoreSource
+from glotter.core.settings import CoreSettings
+from glotter.core.source import CoreSource, CoreSourceCategories, categorize_sources
 from glotter.core.testinfo import ContainerInfo, FolderInfo, TestInfo
 
 EXTENSION_NO_BUILD = ".py"
@@ -121,3 +123,93 @@ def test_test_info_matches_test_info_string(
         filename=filename, language=language, path="some-path", test_info=test_info_string
     )
     assert src.test_info == expected_test_info
+
+
+def test_categorize_sources():
+    settings = CoreSettings()
+    categories = categorize_sources(settings.source_root, settings.projects, CoreSource)
+
+    expected_categories = CoreSourceCategories()
+    testable_languages = ["c-plus-plus", "python"]
+    untestable_languages = ["mathematica"]
+    languages = testable_languages + untestable_languages
+    test_info_strs = {}
+    paths = {}
+    for language in languages:
+        paths[language] = Path(settings.source_root, language[0], language)
+        testinfo_path = paths[language] / "testinfo.yml"
+        test_info_strs[language] = testinfo_path.read_text(encoding="utf-8")
+        expected_categories.test_info[language] = TestInfo.from_dict(
+            yaml.safe_load(test_info_strs[language]), language
+        )
+
+    expected_sources = {
+        "c-plus-plus": {
+            "helloworld": CoreSource(
+                "hello-world.cpp",
+                "c-plus-plus",
+                str(paths["c-plus-plus"]),
+                test_info_strs["c-plus-plus"],
+            )
+        },
+        "mathematica": {
+            "helloworld": CoreSource(
+                "hello-world.nb",
+                "mathematica",
+                str(paths["mathematica"]),
+                test_info_strs["mathematica"],
+            )
+        },
+        "python": {
+            "helloworld": CoreSource(
+                "hello_world.py",
+                "python",
+                str(paths["python"]),
+                test_info_strs["python"],
+            ),
+            "rot13": CoreSource(
+                "rot13.py",
+                "python",
+                str(paths["python"]),
+                test_info_strs["python"],
+            ),
+        },
+    }
+    expected_categories.by_language = {
+        language: [source for source in project_sources.values()]
+        for language, project_sources in expected_sources.items()
+    }
+
+    expected_categories.testable_by_project = {
+        project_type: [] for project_type in settings.projects
+    }
+    for language, project_sources in expected_sources.items():
+        if language in testable_languages:
+            for project_type, source in project_sources.items():
+                expected_categories.testable_by_project[project_type].append(source)
+
+    expected_categories.bad_sources = [
+        str(Path("m", "mathematica", "junk.nb")),
+        str(Path("p", "python", "foo.py")),
+    ]
+    _assert_categorized_sources_eq(categories.by_language, expected_categories.by_language)
+    _assert_categorized_sources_eq(
+        categories.testable_by_project, expected_categories.testable_by_project
+    )
+    assert categories.test_info == expected_categories.test_info
+    assert set(categories.bad_sources) == set(expected_categories.bad_sources)
+
+
+def _assert_categorized_sources_eq(
+    categorized_sources1: dict[str, list[CoreSource]],
+    categorized_sources2: dict[str, list[CoreSource]],
+) -> bool:
+    assert set(categorized_sources1) == set(categorized_sources2)
+    for key, sources in categorized_sources2.items():
+        _assert_sources_eq(categorized_sources1[key], sources)
+
+
+def _assert_sources_eq(sources1: CoreSource, sources2: CoreSource) -> bool:
+    sources1 = sorted(sources1, key=lambda x: x.filename)
+    sources2 = sorted(sources2, key=lambda x: x.filename)
+    assert sources1 == sources2
